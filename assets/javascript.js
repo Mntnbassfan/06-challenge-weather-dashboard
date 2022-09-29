a36c04ddeb9b284ec92b97bacbc7976d79c2dcca
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-09-29 02:25:42 -0600

    added event listener to search button

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,7 @@
 document
   .getElementById("searchButton")
   .addEventListener("click", function (event) {
+    event.preventDefault();
     var userInput = document.getElementById("cityChoice").value;
-    console.log(cityChoice);
+    console.log(userInput);
   });
